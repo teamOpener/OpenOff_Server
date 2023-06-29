eefc36a79a7a799c7d7ea3cbf694a15c64b19fd2
99winnmin <ojysep9987@gmail.com>
2023-06-29 11:25:01 +0900

    [Feat] Open;off project init setting

---
+++ src/main/java/com/example/openoff/OpenoffApplication.java
@@ -0,0 +1,13 @@
+package com.example.openoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenoffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenoffApplication.class, args);
+    }
+
+}
