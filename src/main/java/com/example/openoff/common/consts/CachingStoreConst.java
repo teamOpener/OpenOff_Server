1e9995c9f0d92485967fc86ef67a4ecbbc73e003
99winnmin <ojysep9987@gmail.com>
2023-06-30 19:36:41 +0900

    [Feat] #3 - 캐시메모리 상수 클래스 추가

---
+++ src/main/java/com/example/openoff/common/consts/CachingStoreConst.java
@@ -0,0 +1,12 @@
+package com.example.openoff.common.consts;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CachingStoreConst {
+
+    public static final String COMMENT_CACHE_NAME = "comment";
+    public static final String COMMENT_COUNT_CACHE_NAME = "commentCount";
+
+}
