113508fa3b550b1f1be560284662ecc56de8a51d
99winnmin <ojysep9987@gmail.com>
2023-06-30 19:36:04 +0900

    [Feat] #3 - OpenOff 상수 클래스 추가

---
+++ src/main/java/com/example/openoff/common/consts/ApplicationConst.java
@@ -0,0 +1,16 @@
+package com.example.openoff.common.consts;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ApplicationConst {
+    public static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+    public static final String REFRESH_TOKEN = "REFRESH_TOKEN";
+    public static final String TOKEN_TYPE = "TOKEN_TYPE";
+
+    public static final String JWT_AUTHORIZATION_TYPE = "Bearer"+" ";
+
+    public static final String ACCESS_TOKEN_HEADER = "Authorization";
+    public static final String REFRESH_TOKEN_HEADER = "RefreshToken";
+}
