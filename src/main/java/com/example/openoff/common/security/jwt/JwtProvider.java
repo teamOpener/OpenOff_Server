af9217ba6c0f19e87c242b381ccef27b13d7da70
99winnmin <ojysep9987@gmail.com>
2023-06-30 19:43:18 +0900

    [Feat] #3 - Jwt 생성 구현부

---
+++ src/main/java/com/example/openoff/common/security/jwt/JwtProvider.java
@@ -0,0 +1,129 @@
+package com.example.openoff.common.security.jwt;
+
+import com.example.openoff.common.exception.Error;
+import com.example.openoff.common.security.exception.ExpiredTokenException;
+import com.example.openoff.common.security.exception.InvalidTokenException;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import static com.example.openoff.common.consts.ApplicationConst.*;
+
+/**
+ * TODO : 예외처리에서 사용자 정보를 담어서 로그를 남겨야함, 토큰에 저장할 정보 email로 할지 정하기, refreshToken을 재발급하는 과정을 filter사용할지 아니면 컨트롤러에서 처리할지 정하기
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+
+    private final JwtProperties jwtProperties;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    private Key getSecretKey() {
+        return Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8));
+    }
+
+    private String buildAccessToken(String email, Date now) {
+        return buildToken(now)
+                .setExpiration(new Date(now.getTime() + jwtProperties.getAccessTokenPeriod()))
+                .setSubject(email)
+                .claim(TOKEN_TYPE, ACCESS_TOKEN)
+                .compact();
+    }
+
+    private String buildRefreshToken(String email, Date now) {
+        final String refreshToken = buildToken(now)
+                .setExpiration(new Date(now.getTime() + jwtProperties.getRefreshTokenPeriod()))
+                .setSubject(email)
+                .claim(TOKEN_TYPE, REFRESH_TOKEN)
+                .compact();
+        storeRefreshToken(email, refreshToken);
+        return refreshToken;
+    }
+
+    private void storeRefreshToken(String email, String refreshToken) {
+        redisTemplate.opsForValue().set(
+                email,
+                refreshToken,
+                jwtProperties.getRefreshTokenPeriod(),
+                TimeUnit.MILLISECONDS);
+    }
+
+    private JwtBuilder buildToken(Date now) {
+        final Key key = getSecretKey();
+        return Jwts.builder()
+                .setIssuedAt(now)
+                .signWith(key);
+    }
+
+    public String generateAccessToken(String email) {
+        final Date now = new Date();
+        return buildAccessToken(email, now);
+    }
+
+    public String generateRefreshToken(String email) {
+        final Date now = new Date();
+        return buildRefreshToken(email, now);
+    }
+
+    /**
+     * TODO : 예외 처리 관련해서 좀 더 고민해보기
+     * @throws InvalidTokenException : 유효하지 않은 토큰
+     * @throws ExpiredTokenException : 만료된 토큰
+     * @throws IllegalArgumentException : 토큰이 null일 경우
+     */
+    public void validateToken(String token) {
+        JwtParser jwtParser = Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build();
+        try {
+            jwtParser.parse(token);
+        } catch (MalformedJwtException | SignatureException | IllegalArgumentException e){
+            throw new InvalidTokenException(Error.INVALID_TOKEN, HttpStatus.UNAUTHORIZED);
+        } catch (ExpiredJwtException e){
+            throw new ExpiredTokenException(Error.EXPIRED_TOKEN, HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    public String extractEmail(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public String reIssue(String refreshToken){
+        String email = validateRefreshToken(refreshToken);
+        return generateAccessToken(email);
+    }
+
+    public String validateRefreshToken(String refreshToken) {
+        validateToken(refreshToken);
+        final String email = extractEmail(refreshToken);
+        final String storedRefreshToken = getRefreshToken(email);
+        if(!Objects.equals(refreshToken, storedRefreshToken)){
+            throw new InvalidTokenException(Error.INVALID_TOKEN, HttpStatus.UNAUTHORIZED);
+        }
+        return email;
+    }
+
+    private String getRefreshToken(String email) {
+        return redisTemplate.opsForValue().get(email);
+    }
+
+
+}
