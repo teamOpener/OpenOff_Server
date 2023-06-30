16002db8fbdf2259427243f78386b67de4aee43d
99winnmin <ojysep9987@gmail.com>
2023-06-30 19:34:53 +0900

    [Feat] #3 - SecurityConfig 추가

---
+++ src/main/java/com/example/openoff/common/config/security/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.example.openoff.common.config.security;
+
+import com.example.openoff.common.logging.LoggingFilter;
+import com.example.openoff.common.security.jwt.JwtAuthenticationEntryPoint;
+import com.example.openoff.common.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity // 기본 웹보안 사용
+@RequiredArgsConstructor
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+//    public PasswordEncoder getPasswordEncoder() {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers(
+                        "/v3/api-docs/**",
+                        "/swagger-resources/**",
+                        "/swagger-ui/**");
+    }
+
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .formLogin().disable()
+                .httpBasic().disable();
+
+        return httpSecurity
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests() // http servletRequest 를 사용하는 요청들에 대한 접근제한을 설정
+                .antMatchers("/profile").permitAll()
+                .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
+
+                .and()
+                .csrf().disable()// CSRF 설정 Disable
+                .cors()
+
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthenticationEntryPoint, JwtAuthenticationFilter.class)
+                .addFilterBefore(new LoggingFilter(), JwtAuthenticationEntryPoint.class)
+
+                .build();
+    }
+}
