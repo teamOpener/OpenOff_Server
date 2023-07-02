337c37d9d32c4ba75d3694775d6c025a263bfa5f
99winnmin <ojysep9987@gmail.com>
2023-07-02 13:33:35 +0900

    [Fix] 소셜로그인 관련 에러코드 추가

--- src/main/java/com/example/openoff/common/exception/Error.java
+++ src/main/java/com/example/openoff/common/exception/Error.java
@@ -4,7 +4,20 @@ import lombok.Getter;
 
 @Getter
 public enum Error {
-    DATA_NOT_FOUND("데이터를 찾을 수 없습니다.", 900),
+    DATA_NOT_FOUND("데이터를 찾을 수 없습니다.", 800),
+
+    // OAuth
+    GOOGLE_OAUTH_FAILED("Google에서 토큰을 검색하지 못했습니다.", 900),
+    GOOGLE_OAUTH_FAILED2("Google에서 사용자 정보를 검색하지 못했습니다.", 901),
+    KAKAO_OAUTH_FAILED("KAKAO에서 토큰을 검색하지 못했습니다.", 910),
+    KAKAO_OAUTH_FAILED2("KAKAO에서 사용자 정보를 검색하지 못했습니다.", 911),
+    APPLE_OIDC_FAILED("Apple OAuth Identity Token 형식이 올바르지 않습니다.", 920),
+    APPLE_OIDC_FAILED2("Apple OAuth 로그인 중 Identity Token 유효기간이 만료됐습니다.", 921),
+    APPLE_OIDC_FAILED3("Apple OAuth Identity Token 값이 올바르지 않습니다.", 922),
+    APPLE_OIDC_FAILED4("Apple JWT 값의 alg, kid 정보가 올바르지 않습니다.", 923),
+    APPLE_OIDC_FAILED5("Apple OAuth 통신 암호화 과정 중 문제가 발생했습니다.", 924),
+    APPLE_OIDC_FAILED6("Apple OAuth 로그인 중 public key 생성에 문제가 발생했습니다.", 925),
+    APPLE_OIDC_FAILED7("Apple OAuth Claims 값이 올바르지 않습니다.", 926),
 
     // 사용자
     USER_NOT_FOUND("사용자를 찾을 수 없습니다.", 1000),
