32ba93af4fe6d2266ba539c436542ceafaa9f3e9
99winnmin <ojysep9987@gmail.com>
2023-07-02 13:33:00 +0900

    [Fix] GlobalException 내에 HttpStatus 제거

--- src/main/java/com/example/openoff/common/security/exception/InvalidTokenException.java
+++ src/main/java/com/example/openoff/common/security/exception/InvalidTokenException.java
@@ -1,10 +1,9 @@
 package com.example.openoff.common.security.exception;
 
 import com.example.openoff.common.exception.Error;
-import org.springframework.http.HttpStatus;
 
 public class InvalidTokenException extends JwtException{
-    public InvalidTokenException(Error error, HttpStatus httpStatus) {
-        super(error, httpStatus);
+    public InvalidTokenException(Error error) {
+        super(error);
     }
 }
