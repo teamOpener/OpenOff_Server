acc0f5f7355ce9dd6eb9d7cf32b0b1ef469d01df
99winnmin <ojysep9987@gmail.com>
2023-07-06 03:42:09 +0900

    [Fix] BaseEntity를 위해 @EnableJpaAuditing 추가

--- src/main/java/com/example/openoff/OpenoffApplication.java
+++ src/main/java/com/example/openoff/OpenoffApplication.java
@@ -5,8 +5,10 @@ import com.example.openoff.domain.auth.application.service.google.GoogleOAuthPro
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableConfigurationProperties({JwtProperties.class, GoogleOAuthProperties.class})
 public class OpenoffApplication {
 
