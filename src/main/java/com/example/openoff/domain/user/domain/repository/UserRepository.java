814c2b7c88539cd09392555d2697eb10d302b628
99winnmin <ojysep9987@gmail.com>
2023-07-05 18:59:22 +0900

    [Feat] User 도메인 Repository 추가

---
+++ src/main/java/com/example/openoff/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.openoff.domain.user.domain.repository;
+
+import com.example.openoff.domain.user.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    @Query(value =
+            "select u from User u " +
+            "where u.kakaoAccount.id = :socialId or u.googleAccount.id = :socialId or u.appleAccount.id = :socialId or u.normalAccount.id = :socialId")
+    Optional<User> findBySocialId(Long socialId);
+}
