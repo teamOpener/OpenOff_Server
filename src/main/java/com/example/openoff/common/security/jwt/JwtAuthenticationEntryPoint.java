62c73cc10b2f7a37d5d57c903f203e4dbc9c1fb0
99winnmin <ojysep9987@gmail.com>
2023-07-06 03:37:55 +0900

    [Fix] /auth/** URI 요청 권한 해제

--- src/main/java/com/example/openoff/common/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/openoff/common/security/jwt/JwtAuthenticationEntryPoint.java
@@ -1,11 +1,13 @@
 package com.example.openoff.common.security.jwt;
 
+import com.example.openoff.common.consts.IgnoredPathConst;
 import com.example.openoff.common.exception.dto.ErrorResponse;
 import com.example.openoff.common.security.exception.JwtException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -33,6 +35,18 @@ public class JwtAuthenticationEntryPoint extends OncePerRequestFilter {
             log.info("JwtException: {} {}",e.getError().getMessage(), e.getError().getErrorCode());
             setErrorCode(response, e);
         }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        AntPathMatcher antPathMatcher = new AntPathMatcher();
+        for (String ignoredPath : IgnoredPathConst.IGNORED_PATHS) {
+            if(antPathMatcher.match(ignoredPath, path)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private void setErrorCode(HttpServletResponse response, JwtException e) throws IOException {
