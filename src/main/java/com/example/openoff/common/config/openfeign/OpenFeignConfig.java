72e63d32af7edccafae688311ab3edb725e7541f
99winnmin <ojysep9987@gmail.com>
2023-07-06 03:36:49 +0900

    [Fix] basePackages 재설정

--- src/main/java/com/example/openoff/common/config/openfeign/OpenFeignConfig.java
+++ src/main/java/com/example/openoff/common/config/openfeign/OpenFeignConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients("com.example.openoff")
+@EnableFeignClients(basePackages = {"com.example.openoff"})
 public class OpenFeignConfig {
 
     @Bean
