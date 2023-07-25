6507de1a1c2b454b0d8e7525be9af95678cf3557
99winnmin <ojysep9987@gmail.com>
2023-07-25 11:20:57 +0900

    :rocket: [Feat] eventInfo to SearchMapEventInfoResponseDto mapper 메서드 추가

--- src/main/java/com/example/openoff/domain/eventInstance/application/mapper/EventInstanceMapper.java
+++ src/main/java/com/example/openoff/domain/eventInstance/application/mapper/EventInstanceMapper.java
@@ -3,13 +3,24 @@ package com.example.openoff.domain.eventInstance.application.mapper;
 import com.example.openoff.common.annotation.Mapper;
 import com.example.openoff.domain.eventInstance.application.dto.request.CreateNewEventRequestDto;
 import com.example.openoff.domain.eventInstance.application.dto.response.CreateNewEventResponseDto;
+import com.example.openoff.domain.eventInstance.application.dto.response.SearchMapEventInfoResponseDto;
+import com.example.openoff.domain.eventInstance.domain.entity.EventIndex;
+import com.example.openoff.domain.eventInstance.domain.entity.EventInfo;
+import com.example.openoff.domain.interest.domain.entity.EventInterestField;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper
 public class EventInstanceMapper {
 
-    public static CreateNewEventResponseDto mapToEventInstanceInfoResponse(CreateNewEventRequestDto createDataInfo, Long eventInfoId, List<Long> eventIndexIdList, List<Long> eventImageIdList, List<Long> eventQuestionIdList, List<Long> eventInterestFieldIdList, Long eventStaffId){
+    public static CreateNewEventResponseDto mapToEventInstanceInfoResponse
+            (
+                    CreateNewEventRequestDto createDataInfo,
+                    Long eventInfoId, List<Long> eventIndexIdList, List<Long> eventImageIdList,
+                    List<Long> eventQuestionIdList, List<Long> eventInterestFieldIdList, Long eventStaffId
+            )
+    {
         return CreateNewEventResponseDto.builder()
                 .createDataInfo(createDataInfo)
                 .eventInfoId(eventInfoId)
@@ -19,5 +30,25 @@ public class EventInstanceMapper {
                 .eventInterestFieldIdList(eventInterestFieldIdList)
                 .eventStaffId(eventStaffId)
                 .build();
+    }
+
+    public static SearchMapEventInfoResponseDto mapToSearchMapEventInfoResponse(EventInfo eventInfo){
+        return SearchMapEventInfoResponseDto.builder()
+                .id(eventInfo.getId())
+                .title(eventInfo.getEventTitle())
+                .fieldTypeList(eventInfo.getEventInterestFields().stream().map(EventInterestField::getFieldType).collect(Collectors.toList()))
+                .streetLoadAddress(eventInfo.getLocation().getStreetNameAddress())
+                .detailAddress(eventInfo.getLocation().getDetailAddress())
+                .longitude(eventInfo.getLocation().getLongitude())
+                .latitude(eventInfo.getLocation().getLatitude())
+                .imageList(eventInfo.getEventImages().stream()
+                        .map(eventImage -> SearchMapEventInfoResponseDto.ImageInfo.of(eventImage.getEventImageUrl(), eventImage.getIsMain()))
+                        .collect(Collectors.toList()))
+                .eventDateList(eventInfo.getEventIndexes().stream().map(EventIndex::getEventDate).collect(Collectors.toList()))
+                .build();
+    }
+
+    public static List<SearchMapEventInfoResponseDto> mapToSearchMapEventInfoResponseList(List<EventInfo> eventInfoList){
+        return eventInfoList.stream().map(EventInstanceMapper::mapToSearchMapEventInfoResponse).collect(Collectors.toList());
     }
 }
