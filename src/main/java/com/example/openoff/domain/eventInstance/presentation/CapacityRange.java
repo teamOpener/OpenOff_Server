063ecdabd1809a52d60b0acb062f13771ce8115c
99winnmin <ojysep9987@gmail.com>
2023-07-25 11:23:47 +0900

    :rocket: [Feat] 지도 검색시 인원 수 열거형 정의

---
+++ src/main/java/com/example/openoff/domain/eventInstance/presentation/CapacityRange.java
@@ -0,0 +1,8 @@
+package com.example.openoff.domain.eventInstance.presentation;
+
+public enum CapacityRange {
+    SMALL, // 20인 미만
+    MEDIUM, // 20 ~ 50
+    LARGE, // 50 ~ 100
+    HUGE; // 100 이상
+}
