c3786e22d4c9421a14de28a12bf2fb799b9abe29
99winnmin <ojysep9987@gmail.com>
2023-07-25 11:20:15 +0900

    :rocket: [Feat] 지도 검색 API 추가 및 ModelAttribute 로 파라미터 dto 추가

--- src/main/java/com/example/openoff/domain/eventInstance/presentation/EventInstanceController.java
+++ src/main/java/com/example/openoff/domain/eventInstance/presentation/EventInstanceController.java
@@ -2,16 +2,18 @@ package com.example.openoff.domain.eventInstance.presentation;
 
 import com.example.openoff.common.dto.ResponseDto;
 import com.example.openoff.domain.eventInstance.application.dto.request.CreateNewEventRequestDto;
+import com.example.openoff.domain.eventInstance.application.dto.request.EventSearchRequestDto;
 import com.example.openoff.domain.eventInstance.application.dto.response.CreateNewEventResponseDto;
+import com.example.openoff.domain.eventInstance.application.dto.response.SearchMapEventInfoResponseDto;
 import com.example.openoff.domain.eventInstance.application.service.EventCreateUseCase;
+import com.example.openoff.domain.eventInstance.application.service.EventSearchUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -19,6 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EventInstanceController {
     private final EventCreateUseCase eventCreateUseCase;
+    private final EventSearchUseCase eventSearchUseCase;
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<ResponseDto<List<SearchMapEventInfoResponseDto>>> searchMapEventInfo
+            (@ModelAttribute EventSearchRequestDto eventSearchRequestDto)
+    {
+        log.info("eventSearchRequestDto: {}", eventSearchRequestDto.toString());
+        List<SearchMapEventInfoResponseDto> searchMapEventInfoResponseDtoList = eventSearchUseCase.searchMapEventInfo(eventSearchRequestDto);
+        return ResponseEntity.ok(ResponseDto.of(HttpStatus.OK.value(), "검색 조건에 따라 지도에 이벤트 정보를 불러오는데 성공하였습니다.", searchMapEventInfoResponseDtoList));
+    }
 
     @PostMapping(value = "/create")
     public ResponseEntity<ResponseDto<CreateNewEventResponseDto>> createNewEvent(@RequestBody CreateNewEventRequestDto createNewEventRequestDto) {
