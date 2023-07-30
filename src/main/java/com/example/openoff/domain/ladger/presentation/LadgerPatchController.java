76b8a8328d6832b8044f605aadec56e1382b6631
99winnmin <ojysep9987@gmail.com>
2023-07-30 22:26:29 +0900

    :sparkles: [Feat] 티켓 부분 PATCH API 추가

---
+++ src/main/java/com/example/openoff/domain/ladger/presentation/LadgerPatchController.java
@@ -0,0 +1,33 @@
+package com.example.openoff.domain.ladger.presentation;
+
+import com.example.openoff.common.dto.ResponseDto;
+import com.example.openoff.domain.ladger.application.dto.request.QRCheckRequestDto;
+import com.example.openoff.domain.ladger.application.dto.response.QRCheckResponseDto;
+import com.example.openoff.domain.ladger.application.service.LadgerUpdateUseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/ladger")
+@RequiredArgsConstructor
+public class LadgerPatchController {
+    private final LadgerUpdateUseCase ladgerUpdateUseCase;
+
+    @PatchMapping(value = "/permit")
+    public ResponseEntity<ResponseDto<Void>> applyEvent(@RequestParam Long ladgerId)
+    {
+        ladgerUpdateUseCase.permitAndUpdateQRImageUrl(ladgerId);
+        return ResponseEntity.ok(ResponseDto.of(HttpStatus.OK.value(), "이벤트 신청이 완료되었습니다.", null));
+    }
+
+    @PatchMapping(value = "/qr/check")
+    public ResponseEntity<ResponseDto<QRCheckResponseDto>> applyEvent(@RequestBody QRCheckRequestDto qrCheckRequestDto)
+    {
+        ladgerUpdateUseCase.checkQRCode(qrCheckRequestDto);
+        return ResponseEntity.ok(ResponseDto.of(HttpStatus.OK.value(), "이벤트 신청이 완료되었습니다.", null));
+    }
+}
