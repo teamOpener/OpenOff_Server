f5d92a2e5c8f69351a5eb6bdaeeef62d35807c26
99winnmin <ojysep9987@gmail.com>
2023-08-03 18:26:06 +0900

    :sparkles: [Feat] 자식 댓글 조회 API 추가

--- src/main/java/com/example/openoff/domain/comment/presentation/CommentGetController.java
+++ src/main/java/com/example/openoff/domain/comment/presentation/CommentGetController.java
@@ -2,6 +2,7 @@ package com.example.openoff.domain.comment.presentation;
 
 import com.example.openoff.common.dto.PageResponse;
 import com.example.openoff.common.dto.ResponseDto;
+import com.example.openoff.domain.comment.application.dto.response.ChildCommentInfoResponseDto;
 import com.example.openoff.domain.comment.application.dto.response.ParentCommentInfoResponseDto;
 import com.example.openoff.domain.comment.application.service.CommentSearchUseCase;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/comment")
@@ -22,10 +25,20 @@ public class CommentGetController {
             (
                     @PathVariable Long eventInfoId,
                     @RequestParam(required = false) Long commentId,
-                    @PageableDefault(size = 1) Pageable pageable
+                    @PageableDefault(size = 4) Pageable pageable
             )
     {
         PageResponse<ParentCommentInfoResponseDto> parentCommentsInEvent = commentSearchUseCase.getParentCommentsInEvent(eventInfoId, commentId, pageable);
-        return ResponseEntity.ok(ResponseDto.of(HttpStatus.OK.value(), "댓글 작성이 완료되었습니다.", parentCommentsInEvent));
+        return ResponseEntity.ok(ResponseDto.of(HttpStatus.OK.value(), "댓글 조회가 완료되었습니다.", parentCommentsInEvent));
+    }
+
+    @GetMapping(value = "/child/{eventInfoId}/{commentId}")
+    public ResponseEntity<ResponseDto<List<ChildCommentInfoResponseDto>>> getChildCommentsInEvent
+            (
+                    @PathVariable Long eventInfoId, @PathVariable Long commentId
+            )
+    {
+        List<ChildCommentInfoResponseDto> childCommentsInEvent = commentSearchUseCase.getChildCommentsInEvent(eventInfoId, commentId);
+        return ResponseEntity.ok(ResponseDto.of(HttpStatus.OK.value(), "대댓글 조회 완료되었습니다.", childCommentsInEvent));
     }
 }
