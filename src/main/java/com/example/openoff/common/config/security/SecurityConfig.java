17b2938abf864aaafa1cc6aa29e2475309c3b852
99winnmin <ojysep9987@gmail.com>
2023-08-05 15:01:36 +0900

    :green_heart: [Fix] EB HealthCheck 변경, nginx.conf 수정, firebase 값 추가

--- src/main/java/com/example/openoff/common/config/security/SecurityConfig.java
+++ src/main/java/com/example/openoff/common/config/security/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
 
                 .and()
                 .authorizeRequests() // http servletRequest 를 사용하는 요청들에 대한 접근제한을 설정
-                .antMatchers("/prod").permitAll()
+                .antMatchers("/").permitAll()
                 .antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
--- src/main/java/com/example/openoff/domain/HealthCheckController.java
+++ src/main/java/com/example/openoff/domain/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/prod")
+@RequestMapping("/")
 public class HealthCheckController {
     @GetMapping
     public ResponseEntity<String> hello() {
