bdf3309a6a42a739e44a2e9aa4e738cd3d39c14b
99winnmin <ojysep9987@gmail.com>
2023-08-09 21:32:48 +0900

    :sparkles: [Feat] 카카오 Local REST API 호출 메서드 추가

---
+++ src/main/java/com/example/openoff/domain/eventInstance/application/service/KakaoLocalService.java
@@ -0,0 +1,22 @@
+package com.example.openoff.domain.eventInstance.application.service;
+
+import com.example.openoff.common.config.openfeign.KakaoMapClient;
+import com.example.openoff.domain.eventInstance.application.dto.response.KakaoAddressResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KakaoLocalService {
+    @Value("${oauth.kakao.api-key}")
+    private String API_KEY;
+
+    private final KakaoMapClient kakaoMapClient;
+
+    public KakaoAddressResponse getKakaoAddressToCoord(String query, String analyzeType) {
+        return kakaoMapClient.getKakaoAddressToCoord(query, analyzeType, "KakaoAK " + API_KEY);
+    }
+}
