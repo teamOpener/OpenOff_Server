ee9fe7cb8bf7dfc73ba455dea67657afad342472
99winnmin <ojysep9987@gmail.com>
2023-08-10 11:57:46 +0900

    :sparkles: [Feat] 여러 userId로 User 리스트 조회 메서드 추가

--- src/main/java/com/example/openoff/common/util/UserUtils.java
+++ src/main/java/com/example/openoff/common/util/UserUtils.java
@@ -5,6 +5,8 @@ import com.example.openoff.domain.user.domain.service.UserFindService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class UserUtils {
@@ -14,5 +16,9 @@ public class UserUtils {
     public User getUser(){
         final String userUUID = SecurityUtils.getUserUUID();
         return userFindService.findByUUID(userUUID);
+    }
+
+    public List<User> getUserList(List<String> userIdList){
+        return userFindService.findUsersByUUIDs(userIdList);
     }
 }
--- src/main/java/com/example/openoff/domain/user/domain/service/UserFindService.java
+++ src/main/java/com/example/openoff/domain/user/domain/service/UserFindService.java
@@ -8,6 +8,9 @@ import com.example.openoff.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Slf4j
 @DomainService
 @RequiredArgsConstructor
@@ -22,6 +25,11 @@ public class UserFindService {
     public User findByUUID(String uuid) {
         return userRepository.findById(uuid)
                 .orElseThrow(() -> new UserNotFoundException(Error.USER_NOT_FOUND));
+    }
+
+    public List<User> findUsersByUUIDs(List<String> uuids) {
+        return uuids.stream().map(uuid -> userRepository.findById(uuid)
+                .orElseThrow(() -> UserNotFoundException.of(Error.USER_NOT_FOUND))).collect(Collectors.toList());
     }
 
     public User findByPhoneNum(String phoneNum) {
