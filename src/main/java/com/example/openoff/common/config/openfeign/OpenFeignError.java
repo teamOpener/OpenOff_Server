9e29ab50b94e3f17d3bde3a68d04849fc2380ef0
99winnmin <ojysep9987@gmail.com>
2023-08-10 10:23:43 +0900

    :goal_net: [Chore] 카카오 에러 로깅중...

--- src/main/java/com/example/openoff/common/config/openfeign/OpenFeignError.java
+++ src/main/java/com/example/openoff/common/config/openfeign/OpenFeignError.java
@@ -6,20 +6,40 @@ import feign.Response;
 import feign.codec.ErrorDecoder;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 @Slf4j
 public class OpenFeignError implements ErrorDecoder {
     @Override
     public Exception decode(String methodKey, Response response) {
+        String responseBody = "";
+
+        // 응답 본문을 읽습니다.
+        if (response.body() != null) {
+            try {
+                responseBody = new String(response.body().asInputStream().readAllBytes(), StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                log.error("Failed to read response body", e);
+            }
+        }
+
         switch (response.status()) {
             case 400:
+                logErrorResponse(response, responseBody);
                 return BusinessException.of(Error.DATA_NOT_FOUND);
             case 401:
-                log.info(response.reason());
-                log.info(response.toString());
-                log.info(response.body().toString());
+                logErrorResponse(response, responseBody);
                 return BusinessException.of(Error.DATA_NOT_FOUND);
+            // 필요한 다른 상태 코드에 대한 처리를 추가할 수 있습니다.
         }
         return null;
+    }
+
+    private void logErrorResponse(Response response, String responseBody) {
+        log.info("URL: {}", response.request().url());
+        log.info("Headers: {}", response.request().headers());
+        log.info("Response Body: {}", responseBody);
     }
 
 }
