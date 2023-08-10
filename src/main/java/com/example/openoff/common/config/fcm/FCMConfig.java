98d4fa29a23c97f89f6653c2f8f2169a4d0f473a
99winnmin <ojysep9987@gmail.com>
2023-08-10 22:18:11 +0900

    :sparkles: [Feat] Fcm Config & 기본 Service 메서드 추가

--- src/main/java/com/example/openoff/common/config/fcm/FCMConfig.java
+++ src/main/java/com/example/openoff/common/config/fcm/FCMConfig.java
@@ -1,5 +1,7 @@
 package com.example.openoff.common.config.fcm;
 
+import com.example.openoff.common.exception.BusinessException;
+import com.example.openoff.common.exception.Error;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -15,27 +17,31 @@ import java.util.List;
 @Configuration
 public class FCMConfig {
     @Bean
-    FirebaseMessaging firebaseMessaging() throws IOException {
-        ClassPathResource resource = new ClassPathResource("firebase/openoff-484dd-firebase-adminsdk-ruh1l-e458eb4d19.json");
+    FirebaseMessaging firebaseMessaging() {
+        try {
+            ClassPathResource resource = new ClassPathResource("firebase/openoff-484dd-firebase-adminsdk-ruh1l-e458eb4d19.json");
 
-        InputStream refreshToken = resource.getInputStream();
+            InputStream refreshToken = resource.getInputStream();
 
-        FirebaseApp firebaseApp = null;
-        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+            FirebaseApp firebaseApp = null;
+            List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
 
-        if (firebaseAppList != null && !firebaseAppList.isEmpty()){
-            for (FirebaseApp app : firebaseAppList){
-                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)){
-                    firebaseApp = app;
+            if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
+                for (FirebaseApp app : firebaseAppList) {
+                    if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                        firebaseApp = app;
+                    }
                 }
-            }
-        } else {
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
-                    .build();
+            } else {
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                        .build();
 
-            firebaseApp = FirebaseApp.initializeApp(options);
+                firebaseApp = FirebaseApp.initializeApp(options);
+            }
+            return FirebaseMessaging.getInstance(firebaseApp);
+        } catch (IOException e) {
+            throw BusinessException.of(Error.INTERNAL_SERVER_ERROR);
         }
-        return FirebaseMessaging.getInstance(firebaseApp);
     }
 }
---
+++ src/main/java/com/example/openoff/common/infrastructure/fcm/FirebaseService.java
@@ -0,0 +1,103 @@
+package com.example.openoff.common.infrastructure.fcm;
+
+import com.example.openoff.domain.user.domain.entity.User;
+import com.google.firebase.messaging.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class FirebaseService {
+    private final FirebaseMessaging firebaseMessaging;
+
+    // Android 세팅
+    private AndroidConfig androidConfig(String title, String body) {
+        return AndroidConfig.builder()
+                .setNotification(AndroidNotification.builder()
+                        .setTitle(title)
+                        .setBody(body)
+                        .build())
+                .build();
+    }
+
+    // APNs 세팅 ( iOS )
+    private ApnsConfig apnsConfig(String title, String body) {
+        return ApnsConfig.builder()
+                .setAps(Aps.builder()
+                        .setAlert(
+                                ApsAlert.builder()
+                                        .setTitle(title)
+                                        .setBody(body)
+                                        .build()
+                        )
+                        .setSound("default")
+                        .build())
+                .build();
+    }
+
+    public void sendFCMNotificationSingle(User user, String title, String body) {
+        if (user.getFcmToken() == null) return;
+        Notification notification = Notification.builder()
+                .setTitle(title).setBody(body).build();
+
+        Message message = Message.builder()
+                .setAndroidConfig(androidConfig(title, body))
+                .setApnsConfig(apnsConfig(title, body))
+                .setNotification(notification)
+                .setToken(user.getFcmToken())
+                .build();
+        try {
+            firebaseMessaging.send(message);
+        } catch (Exception e) {
+            return;
+        }
+    }
+
+    public void sendFCMNotificationMulticast(List<User> users, String title, String body) {
+        Notification notification = Notification.builder()
+                .setTitle(title).setBody(body).build();
+        MulticastMessage multicastMessage = MulticastMessage.builder()
+                .setNotification(notification)
+                .setAndroidConfig(androidConfig(title, body))
+                .setApnsConfig(apnsConfig(title, body))
+                .addAllTokens(users.stream()
+                        .map(User::getFcmToken)
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toList()))
+                .build();
+
+        firebaseMessaging.sendMulticastAsync(multicastMessage);
+    }
+
+    public void sendToTopic(String topic, String title, String body) {
+        Notification notification = Notification.builder()
+                .setTitle(title).setBody(body).build();
+
+        Message message = Message.builder()
+                .setAndroidConfig(androidConfig(title, body))
+                .setApnsConfig(apnsConfig(title, body))
+                .setNotification(notification)
+                .setTopic(topic)
+                .build();
+
+        firebaseMessaging.sendAsync(message);
+    }
+
+    public void subScribe(String topicName, List<String> fcmTokenList) {
+        firebaseMessaging.subscribeToTopicAsync(
+                fcmTokenList,
+                topicName
+        );
+    }
+
+    public void unSubscribe(String topicName, List<String> fcmTokenList) {
+        firebaseMessaging.unsubscribeFromTopicAsync(
+                fcmTokenList,
+                topicName
+        );
+    }
+}
