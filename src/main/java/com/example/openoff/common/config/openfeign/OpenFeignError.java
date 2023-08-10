3f01a73fa89e9d3885eba9237ac6c66526d9e9f2
99winnmin <ojysep9987@gmail.com>
2023-08-10 09:49:30 +0900

    :boom: [Chore] log 찍기

--- src/main/java/com/example/openoff/common/config/openfeign/OpenFeignError.java
+++ src/main/java/com/example/openoff/common/config/openfeign/OpenFeignError.java
@@ -4,12 +4,19 @@ import com.example.openoff.common.exception.BusinessException;
 import com.example.openoff.common.exception.Error;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class OpenFeignError implements ErrorDecoder {
     @Override
     public Exception decode(String methodKey, Response response) {
         switch (response.status()) {
             case 400:
+                return BusinessException.of(Error.DATA_NOT_FOUND);
+            case 401:
+                log.info(response.reason());
+                log.info(response.toString());
+                log.info(response.body().toString());
                 return BusinessException.of(Error.DATA_NOT_FOUND);
         }
         return null;
