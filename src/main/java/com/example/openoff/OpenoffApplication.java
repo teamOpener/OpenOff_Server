bbd6241dc032611429002c38a201b8c4b3b7a55a
99winnmin <ojysep9987@gmail.com>
2023-08-13 15:14:40 +0900

    :zap: [Fix] quartz 스케쥴러 허용

--- src/main/java/com/example/openoff/OpenoffApplication.java
+++ src/main/java/com/example/openoff/OpenoffApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties({JwtProperties.class, GoogleOAuthProperties.class, NCPSmsProperties.class})
