183613e31a15c1e159da8c63f7f9ce8a9e8e05b9
99winnmin <ojysep9987@gmail.com>
2023-08-13 15:17:35 +0900

    :sparkles: [Feat] NotificationType Repository 추기

---
+++ src/main/java/com/example/openoff/domain/notification/domain/repository/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.example.openoff.domain.notification.domain.repository;
+
+import com.example.openoff.domain.notification.domain.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
