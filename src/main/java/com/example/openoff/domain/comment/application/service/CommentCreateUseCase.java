6c4448ea726c1318c88f8c80633084c6ed82d3bd
99winnmin <ojysep9987@gmail.com>
2023-08-13 19:09:18 +0900

    :bug: [Feat] 문의 댓글 달렸을 때 Staff에게 푸쉬알림 가도록 설정

--- src/main/java/com/example/openoff/domain/comment/application/service/CommentCreateUseCase.java
+++ src/main/java/com/example/openoff/domain/comment/application/service/CommentCreateUseCase.java
@@ -9,11 +9,15 @@ import com.example.openoff.domain.comment.domain.entity.EventComment;
 import com.example.openoff.domain.comment.domain.service.CommentService;
 import com.example.openoff.domain.eventInstance.domain.entity.EventInfo;
 import com.example.openoff.domain.eventInstance.domain.service.EventInfoService;
+import com.example.openoff.domain.ladger.domain.entity.EventStaff;
+import com.example.openoff.domain.notification.application.service.NotificationCreateService;
 import com.example.openoff.domain.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @UseCase
@@ -23,11 +27,16 @@ public class CommentCreateUseCase {
     private final UserUtils userUtils;
     private final CommentService commentService;
     private final EventInfoService eventInfoService;
+    private final NotificationCreateService notificationCreateService;
 
     public CommentWriteResponseDto insert(CommentWriteRequestDto commentWriteRequestDTO) {
         User user = userUtils.getUser();
         EventInfo eventInfo = eventInfoService.findEventInfoById(commentWriteRequestDTO.getEventInfoId());
+        List<User> staffs = eventInfo.getEventStaffs().stream().map(EventStaff::getStaff).collect(Collectors.toList());
 
+        if (commentWriteRequestDTO.getParentId() == null && !staffs.contains(user)) {
+            notificationCreateService.createCommentNotificationToStaff(staffs, eventInfo.getId(), commentWriteRequestDTO);
+        }
         EventComment comment = commentService.insert(user, eventInfo, commentWriteRequestDTO);
         return CommentMapper.mapToCommentWriteResDto(comment, commentWriteRequestDTO);
     }
