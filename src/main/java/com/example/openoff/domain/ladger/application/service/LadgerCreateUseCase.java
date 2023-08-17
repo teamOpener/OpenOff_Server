871680206eb8de2c0870600f7f269f6ed3b4a633
99winnmin <ojysep9987@gmail.com>
2023-08-17 12:13:51 +0900

    :rocket: [Fix] 이벤트 신청 시 fcm 로직 구상중...

--- src/main/java/com/example/openoff/domain/ladger/application/service/LadgerCreateUseCase.java
+++ src/main/java/com/example/openoff/domain/ladger/application/service/LadgerCreateUseCase.java
@@ -1,6 +1,8 @@
 package com.example.openoff.domain.ladger.application.service;
 
 import com.example.openoff.common.annotation.UseCase;
+import com.example.openoff.common.exception.BusinessException;
+import com.example.openoff.common.exception.Error;
 import com.example.openoff.common.util.UserUtils;
 import com.example.openoff.domain.eventInstance.domain.entity.EventExtraQuestion;
 import com.example.openoff.domain.eventInstance.domain.entity.EventIndex;
@@ -9,6 +11,7 @@ import com.example.openoff.domain.eventInstance.domain.service.EventExtraQuestio
 import com.example.openoff.domain.eventInstance.domain.service.EventIndexService;
 import com.example.openoff.domain.ladger.application.dto.request.ApplyEventRequestDto;
 import com.example.openoff.domain.ladger.domain.service.EventApplicantLadgerService;
+import com.example.openoff.domain.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,15 +31,27 @@ public class LadgerCreateUseCase {
     private final EventExtraAnswerService eventExtraAnswerService;
 
     public void createEventApplicationLadger(ApplyEventRequestDto applyEventRequestDto) {
+        User user = userUtils.getUser();
         EventIndex eventIndex = eventIndexService.findById(applyEventRequestDto.getEventIndexId());
-        eventApplicantLadgerService.createEventApplicantLadger(eventIndex, userUtils.getUser());
+        if (!eventIndex.getIsClose() && eventIndex.getEventInfo().getEventApplyPermit()){
+            int applicantNum = eventApplicantLadgerService.findInEventIndex(eventIndex.getId()).size();
+            int totalRegisterCount = eventIndex.getEventInfo().getTotalRegisterCount();
+            // TODO: 정책 논의 후 topic 정의
+//            if (applicantNum + 1 >= (totalRegisterCount/2)) {
+//
+//            }
+            eventApplicantLadgerService.createEventApplicantLadger(eventIndex, userUtils.getUser());
 
-        List<ApplyEventRequestDto.AnswerInfo> answerInfoList = applyEventRequestDto.getAnswerInfoList();
+            List<ApplyEventRequestDto.AnswerInfo> answerInfoList = applyEventRequestDto.getAnswerInfoList();
 
-        List<EventExtraQuestion> extraQuestions = eventExtraQuestionService.findExtraQuestions(
-                answerInfoList.stream().map(ApplyEventRequestDto.AnswerInfo::getEventExtraQuestionId
-                ).collect(Collectors.toList()));
+            List<EventExtraQuestion> extraQuestions = eventExtraQuestionService.findExtraQuestions(
+                    answerInfoList.stream().map(ApplyEventRequestDto.AnswerInfo::getEventExtraQuestionId
+                    ).collect(Collectors.toList()));
 
-        eventExtraAnswerService.createEventExtraAnswers(applyEventRequestDto, userUtils.getUser(), extraQuestions);
+            eventExtraAnswerService.createEventExtraAnswers(applyEventRequestDto, user, extraQuestions);
+        } else {
+            throw BusinessException.of(Error.EVENT_CLOSED);
+        }
+
     }
 }
