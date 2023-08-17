c93ec9f13a16f05c81ef413afe6b0bb814bfbbe3
99winnmin <ojysep9987@gmail.com>
2023-08-17 12:06:34 +0900

    :bug: [Feat] 스프링 배치 및 스케쥴러 활성화

--- src/main/java/com/example/openoff/OpenoffApplication.java
+++ src/main/java/com/example/openoff/OpenoffApplication.java
@@ -3,16 +3,19 @@ package com.example.openoff;
 import com.example.openoff.common.security.jwt.JwtProperties;
 import com.example.openoff.domain.auth.application.service.google.GoogleOAuthProperties;
 import com.example.openoff.domain.auth.application.service.sms.NCPSmsProperties;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = "com.example.openoff")
 @EnableJpaAuditing
 @EnableConfigurationProperties({JwtProperties.class, GoogleOAuthProperties.class, NCPSmsProperties.class})
+@EnableScheduling
+@EnableBatchProcessing
 public class OpenoffApplication {
 
     public static void main(String[] args) {
