df311458fa1197bbb27c719c5dd4c0c9aa99ed4f
99winnmin <ojysep9987@gmail.com>
2023-08-19 15:36:36 +0900

    :necktie: [Feat] 스태프가 신청 내역 삭제 비지니스 로직 추가

--- src/main/java/com/example/openoff/domain/ladger/application/service/LadgerDeleteUseCase.java
+++ src/main/java/com/example/openoff/domain/ladger/application/service/LadgerDeleteUseCase.java
@@ -1,13 +1,19 @@
 package com.example.openoff.domain.ladger.application.service;
 
 import com.example.openoff.common.annotation.UseCase;
+import com.example.openoff.common.exception.BusinessException;
+import com.example.openoff.common.exception.Error;
 import com.example.openoff.common.util.UserUtils;
+import com.example.openoff.domain.eventInstance.domain.service.EventExtraAnswerService;
 import com.example.openoff.domain.ladger.domain.entity.EventApplicantLadger;
 import com.example.openoff.domain.ladger.domain.service.EventApplicantLadgerService;
 import com.example.openoff.domain.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @UseCase
@@ -16,11 +22,26 @@ import org.springframework.transaction.annotation.Transactional;
 public class LadgerDeleteUseCase {
     private final UserUtils userUtils;
     private final EventApplicantLadgerService eventApplicantLadgerService;
+    private final EventExtraAnswerService eventExtraAnswerService;
+
 
     public void deleteMyApplyLadger(Long ladgerId){
         User user = userUtils.getUser();
         EventApplicantLadger myEventTicketInfo = eventApplicantLadgerService.getApplicationInfo(ladgerId, user.getId());
         myEventTicketInfo.getEventInfo().updateTotalRegisterCountMinus();
+        eventExtraAnswerService.deleteEventExtraAnswers(myEventTicketInfo.getEventIndex().getId());
         eventApplicantLadgerService.deleteEventApplicantLadger(myEventTicketInfo.getId());
+    }
+
+    public void deleteRejectLadger(Long ladgerId) {
+        User user = userUtils.getUser();
+        EventApplicantLadger ladgerInfo = eventApplicantLadgerService.findLadgerInfo(ladgerId);
+        List<String> staffId = ladgerInfo.getEventInfo().getEventStaffs().stream()
+                .map(staff -> staff.getStaff().getId()).collect(Collectors.toList());
+        if (!staffId.contains(user.getId())) {
+            throw BusinessException.of(Error.EVENT_STAFF_NOT_FOUND);
+        }
+        ladgerInfo.getEventInfo().updateTotalRegisterCount();
+        eventApplicantLadgerService.deleteEventApplicantLadger(ladgerId);
     }
 }
