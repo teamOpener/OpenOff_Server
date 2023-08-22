dd5943f9161982eb0667613b4e37606dcb5f52bd
99winnmin <ojysep9987@gmail.com>
2023-08-22 09:09:04 +0900

    :bricks: [Feat] 알림 조희 쿼리 추가

--- src/main/java/com/example/openoff/domain/notification/domain/repository/NotificationRepositoryCustom.java
+++ src/main/java/com/example/openoff/domain/notification/domain/repository/NotificationRepositoryCustom.java
@@ -1,6 +1,10 @@
 package com.example.openoff.domain.notification.domain.repository;
 
+import com.example.openoff.domain.notification.domain.entity.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface NotificationRepositoryCustom {
     Boolean existsNotificationInSpecificCase(String userId, String content, Long notificationParameter);
-
+    Page<Notification> getNotificationList(Long notificationId, String userId, Pageable pageable);
 }
--- src/main/java/com/example/openoff/domain/notification/infrastructure/NotificationRepositoryImpl.java
+++ src/main/java/com/example/openoff/domain/notification/infrastructure/NotificationRepositoryImpl.java
@@ -1,9 +1,17 @@
 package com.example.openoff.domain.notification.infrastructure;
 
+import com.example.openoff.domain.notification.domain.entity.Notification;
 import com.example.openoff.domain.notification.domain.repository.NotificationRepositoryCustom;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 import static com.example.openoff.domain.notification.domain.entity.QNotification.notification;
 
@@ -23,5 +31,32 @@ public class NotificationRepositoryImpl implements NotificationRepositoryCustom 
                                 .and(notification.notificationParameter.eq(notificationParameter))
                 )
                 .fetchFirst() != null;
+    }
+
+    @Override
+    public Page<Notification> getNotificationList(Long notificationId, String userId, Pageable pageable) {
+        List<Notification> notifications = queryFactory
+                .select(notification)
+                .from(notification)
+                .where(
+                        notification.user.id.eq(userId),
+                        ltNotificationId(notificationId)
+                )
+                .orderBy(notification.id.desc())
+                .limit(pageable.getPageSize())
+                .fetch();
+        JPAQuery<Long> countQuery = queryFactory
+                .select(notification.count())
+                .from(notification)
+                .where(
+                        notification.user.id.eq(userId),
+                        ltNotificationId(notificationId)
+                );
+
+        return PageableExecutionUtils.getPage(notifications, pageable, countQuery::fetchOne);
+    }
+
+    private BooleanExpression ltNotificationId(Long notificationId) {
+        return notificationId != null ? notification.id.lt(notificationId) : null;
     }
 }
