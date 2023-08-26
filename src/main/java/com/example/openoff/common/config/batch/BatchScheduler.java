2b04b0b30cf662c09e8cf08c99700037494a52b6
99winnmin <ojysep9987@gmail.com>
2023-08-26 10:57:37 +0900

    :sparkles: [Feat] 누락된 BatchScheduler 추가

--- src/main/java/com/example/openoff/common/config/batch/BatchScheduler.java
+++ src/main/java/com/example/openoff/common/config/batch/BatchScheduler.java
@@ -16,23 +16,11 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class BatchScheduler {
     private final JobLauncher jobLauncher;
+    private final EventApplicantBatchConfig eventApplicantBatchConfig;
     private final BookmarkEventBatchConfig bookmarkEventBatchConfig;
     private final EventStaffBatchConfig eventStaffBatchConfig;
     private final AsyncConfig asyncConfig;
 
-
-
-    @Scheduled(cron = "0 0 9 * * *")
-    public void bookmarkEventApplyLeft1DayJob() {
-        Map<String, JobParameter> confMap = Map.of("time", new JobParameter(System.currentTimeMillis()));
-        JobParameters jobParameters = new JobParameters(confMap);
-
-        try{
-            jobLauncher.run(bookmarkEventBatchConfig.bookmarkEventJob(), jobParameters);
-        } catch (Exception e) {
-            log.error("bookmarkEventApplyLeft1DayJob error : {}", e.getMessage());
-        }
-    }
 
     @Scheduled(cron = "0 0 7 * * *")
     public void eventStaffJob() {
@@ -69,6 +57,44 @@ public class BatchScheduler {
             jobLauncher.run(eventStaffBatchConfig.eventStaffCountNotApprovedApplicantJob(), jobParameters);
         } catch (Exception e) {
             log.error("bookmarkEventApplyLeft1DayJob error : {}", e.getMessage());
+        }
+    }
+
+    @Scheduled(cron = "0 0 9 * * *")
+    public void bookmarkEventApplyLeft1DayJob() {
+        Map<String, JobParameter> confMap = Map.of("time", new JobParameter(System.currentTimeMillis()));
+        JobParameters jobParameters = new JobParameters(confMap);
+
+        try{
+            jobLauncher.run(bookmarkEventBatchConfig.bookmarkEventJob(), jobParameters);
+        } catch (Exception e) {
+            log.error("bookmarkEventApplyLeft1DayJob error : {}", e.getMessage());
+        }
+    }
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void eventApplicant1DayOrDDayLeftJob() {
+        Map<String, JobParameter> confMap = Map.of("time", new JobParameter(System.currentTimeMillis()));
+        JobParameters jobParameters = new JobParameters(confMap);
+
+        try {
+            asyncConfig.taskExecutor().execute(() -> {
+                try {
+                    jobLauncher.run(eventApplicantBatchConfig.eventApplicantLeft1DayJob(), jobParameters);
+                } catch (Exception e) {
+                    log.error("Error running eventLeft1DayJob : {}", e.getMessage());
+                }
+            });
+
+            asyncConfig.taskExecutor().execute(() -> {
+                try {
+                    jobLauncher.run(eventApplicantBatchConfig.eventApplicantLeftDDayJob(), jobParameters);
+                } catch (Exception e) {
+                    log.error("Error running eventLeftDDayJob : {}", e.getMessage());
+                }
+            });
+        } catch (Exception e) {
+            log.error("eventStaffJob error : {}", e.getMessage());
         }
     }
 }
